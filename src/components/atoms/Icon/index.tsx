26a2804ccc5ff2857767eff77ceeb859c87cd759
camilaffonseca <camila.fonseca@nodo.cc>
2021-08-29 15:32:00 -0300

    feat: created Text and Icon components

---
+++ src/components/atoms/Icon/index.tsx
@@ -0,0 +1,7 @@
+import styled from '@xstyled/styled-components'
+
+const Icon = styled.iBox`
+  color: gray;
+`
+
+export default Icon
---
+++ src/components/atoms/Text/index.tsx
@@ -0,0 +1,7 @@
+import styled from '@xstyled/styled-components'
+
+const Text = styled.pBox`
+  font-size: 1.4rem;
+`
+
+export default Text
