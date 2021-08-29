83cca0b2d3f5d93a0c638866393f2c966094b237
camilaffonseca <camila.fonseca@nodo.cc>
2021-08-29 15:27:15 -0300

    feat: added global style and theme in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
 import type { AppProps } from 'next/app'
 
+import { ThemeProvider } from '@xstyled/styled-components'
+
+import GlobalStyle from 'theme/GlobalStyle'
+import theme from 'theme/theme'
 import 'theme/preflight.css'
 
-const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />
+const App = ({ Component, pageProps }: AppProps) => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <Component {...pageProps} />
+  </ThemeProvider>
+)
 
 export default App
