3ea51f0a4b42f7b4b310484cfaf17f756825c8db
camilaffonseca <camila.fonseca@nodo.cc>
2021-08-30 07:05:50 -0300

    feat: upgraded Header

--- src/components/atoms/Header/Header.tsx
+++ src/components/atoms/Header/Header.tsx
@@ -2,11 +2,12 @@ import styled, { th } from '@xstyled/styled-components'
 
 const Header = styled.headerBox`
   background-color: white;
-  border-bottom: 1px solid ${th.color('black12')};
-  padding: 0 20px;
+  border-bottom: 0.1rem solid ${th.color('black12')};
+  padding: 1.6rem;
   align-items: center;
-  height: 56px;
+  height: 5.6rem;
   width: 100%;
+  display: flex;
 `
 
 export default Header
